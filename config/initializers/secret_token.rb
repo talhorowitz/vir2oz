dd97b9fa3227a449a97e01716882ce615206197b
talhorowitz <tal.horowitz@vir2oz.com>
2012-10-17 10:47:42 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vir2oz::Application.config.secret_token = '96516a15b67217f1e33b6bf624ba77191bf24ac5d0edef95cf7579d33d5126ab12ee9204015179ca179338950e598c7659eb75c1bba450ff898d5f2cb836194c'
